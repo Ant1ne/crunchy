10c171cff21fcadf570ff2e8177e1ad9e5602b24
Antoine <antoine.aubard@gmail.com>
2024-02-19 16:59:17 +0100

    refactored the GoogleMap component

--- components/GoogleMap_.js
+++ components/GoogleMap_.js
@@ -4,6 +4,8 @@ import { UserLocationContext } from "../context/UserLocationContext";
 import Marker from "./Marker";
 import { SelectedBusinessContext } from "../context/SelectedBusinessContext";
 
+const GOOGLE_MAPS_API_KEY = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
+
 function GoogleMap_() {
   const { userLocation, setUserLocation } = useContext(UserLocationContext);
   const { selectedBusiness, setSelectedBusiness } = useContext(
@@ -15,27 +17,28 @@ function GoogleMap_() {
     borderRadius: 20,
   };
 
+  if (!userLocation) {
+    return null;
+  }
+
+  const center =
+    !selectedBusiness || !selectedBusiness.geometry
+      ? {
+          lat: userLocation.lat,
+          lng: userLocation.lng,
+        }
+      : selectedBusiness.geometry.location;
+
   return (
     <div>
-      <LoadScript googleMapsApiKey={process.env.NEXT_PUBLIC_GOOGLE_API_KEY}>
-        {userLocation ? (
-          <GoogleMap
-            mapContainerStyle={containerStyle}
-            center={
-              !selectedBusiness.name
-                ? {
-                    lat: parseFloat(userLocation.lat),
-                    lng: parseFloat(userLocation.lng),
-                  }
-                : selectedBusiness.geometry.location
-            }
-            zoom={selectedBusiness.name ? 15 : 10}
-          >
-            <>
-              <Marker userLocation={userLocation} />
-            </>
-          </GoogleMap>
-        ) : null}
+      <LoadScript googleMapsApiKey={GOOGLE_MAPS_API_KEY}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          center={center}
+          zoom={selectedBusiness ? 15 : 10}
+        >
+          <Marker userLocation={userLocation} />
+        </GoogleMap>
       </LoadScript>
     </div>
   );
